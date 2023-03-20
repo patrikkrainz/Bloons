b82b569a2b84d65fed4bc923658f59de7f089fff
Der 13. Apostel <92331055+patrikkrainz@users.noreply.github.com>
2023-03-20 14:22:53 +0100

    Targetfarbe
    Das Target kann jetzt in mehreren Farben spawnen und es wird berücksichtig welche es ist sodass sie nicht doppelt spawnen können
    (NOCH NICHT GETESTET!!!)

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -21,6 +21,8 @@ public class GameHandler : MonoBehaviour
 
     public int Level = 1;
     public int BalloonCap;
+    public int TargetBalloonIndex;
+    public int BalloonIndex;
 
     public static float gameTimer = 10;
 
@@ -86,7 +88,9 @@ public class GameHandler : MonoBehaviour
             {
                 if(i == 0)
                 {
-                    force = new Vector3(Random.Range(0, 2), Random.Range(0, 2), Random.Range(0, 2));
+                    TargetBalloonIndex = Random.Range(0, 4);
+
+                    force = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
 
                     posZ = Random.Range(0, 6.6f);
 
@@ -122,53 +126,61 @@ public class GameHandler : MonoBehaviour
                     }
 
                     spawnPosition = new Vector3(posX, posY, posZ);
-                    currentBalloon = Instantiate(Balloons[0], spawnPosition, Quaternion.identity);
+                    currentBalloon = Instantiate(Balloons[TargetBalloonIndex], spawnPosition, Quaternion.identity);
                     rb = currentBalloon.GetComponent<Rigidbody>();
                     rb.AddForce(force);
                 }
                 else
                 {
-                    int balloonType = Random.Range(1, 4);
+                    BalloonIndex = Random.Range(0, 4);
 
-                    force = new Vector3(Random.Range(0, 2), Random.Range(0, 2), Random.Range(0, 2));
+                    if(BalloonIndex == TargetBalloonIndex)
+                    {
+                        BalloonIndex = Random.Range(1, 4);
+                        i-=1;
+                    }
+                    else
+                    {
+                        force = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
 
-                    posZ = Random.Range(0, 6.6f);
+                        posZ = Random.Range(0, 6.6f);
 
-                    if(posZ < 1)
-                    {
-                        posX = Random.Range(-4.2f, 4.6f);
-                        posY = Random.Range(-1, 3);
-                    }
-                    else if(posZ >= 1 && posZ < 2)
-                    {
-                        posX = Random.Range(-5.4f, 5.8f);
-                        posY = Random.Range(-1, 3);
-                    }
-                    else if (posZ >= 2 && posZ < 3)
-                    {
-                        posX = Random.Range(-6.6f, 7);
-                        posY = Random.Range(-1, 3);
-                    }
-                    else if (posZ >= 3 && posZ < 4)
-                    {
-                        posX = Random.Range(-7.8f, 8.2f);
-                        posY = Random.Range(-1, 3);
-                    }
-                    else if (posZ >= 4 && posZ < 5)
-                    {
-                        posX = Random.Range(-9, 9.4f);
-                        posY = Random.Range(-1, 3);
-                    }
-                    else if (posZ >= 5 && posZ < 6)
-                    {
-                        posX = Random.Range(-10.2f, 10.6f);
-                        posY = Random.Range(-1, 3);
-                    }
+                        if (posZ < 1)
+                        {
+                            posX = Random.Range(-4.2f, 4.6f);
+                            posY = Random.Range(-1, 3);
+                        }
+                        else if (posZ >= 1 && posZ < 2)
+                        {
+                            posX = Random.Range(-5.4f, 5.8f);
+                            posY = Random.Range(-1, 3);
+                        }
+                        else if (posZ >= 2 && posZ < 3)
+                        {
+                            posX = Random.Range(-6.6f, 7);
+                            posY = Random.Range(-1, 3);
+                        }
+                        else if (posZ >= 3 && posZ < 4)
+                        {
+                            posX = Random.Range(-7.8f, 8.2f);
+                            posY = Random.Range(-1, 3);
+                        }
+                        else if (posZ >= 4 && posZ < 5)
+                        {
+                            posX = Random.Range(-9, 9.4f);
+                            posY = Random.Range(-1, 3);
+                        }
+                        else if (posZ >= 5 && posZ < 6)
+                        {
+                            posX = Random.Range(-10.2f, 10.6f);
+                            posY = Random.Range(-1, 3);
+                        }
 
-                    spawnPosition = new Vector3(posX, posY, posZ);
-                    currentBalloon = Instantiate(Balloons[balloonType], spawnPosition, Quaternion.identity);
-                    rb = currentBalloon.GetComponent<Rigidbody>();
-                    rb.AddForce(force);
+                        spawnPosition = new Vector3(posX, posY, posZ);
+                        currentBalloon = Instantiate(Balloons[BalloonIndex], spawnPosition, Quaternion.identity);
+                        rb = currentBalloon.GetComponent<Rigidbody>();
+                        rb.AddForce(force);
+                    }
                 }
             }
         }
